659c7d80b3860b7e1b33995395ef5b07afdbcbc4
OlesyaF <sorglos_engel@mail.ru>
2018-06-12 16:37:13 +0300

    Задание 2. Часть 1. Класс Point, функция distance, запускаемый класс

--- sandbox/src/main/java/ru/pdt53/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/pdt53/sandbox/MyFirstProgram.java
@@ -3,22 +3,21 @@ package ru.pdt53.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    hello("world");
-    hello("Olesya");
 
-    Square s = new Square(5);
-    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+    Point p1 = new Point();
+    p1.x = 10;
+    p1.y = 30;
 
-    Rectangle r = new Rectangle(6, 4);
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+    Point p2 = new Point();
+    p2.x = 20;
+    p2.y = 30;
+
+    System.out.println("Расстояние между двумя точками с координатами " + "(" + p1.x + "," + p1.y + ") и (" + p2.x + "," + p2.y + ") равно " + distance(p1, p2));
+
   }
 
-  public static void hello(String somebody) {
-    System.out.println("Hello, " + somebody + "!");
-  }
-
-  public static double area(Rectangle r) {
-    return r.a * r.b;
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
   }
 
 }
---
+++ sandbox/src/main/java/ru/pdt53/sandbox/Point.java
@@ -0,0 +1,6 @@
+package ru.pdt53.sandbox;
+
+public class Point {
+  double x;
+  double y;
+}
