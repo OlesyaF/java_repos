5f9b8bf59ceb835544c3a7c75e2c47c0f0aacaa2
OlesyaF <sorglos_engel@mail.ru>
2018-06-12 14:31:59 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/pdt53/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/pdt53/sandbox/MyFirstProgram.java
@@ -3,8 +3,28 @@ package ru.pdt53.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("world");
+    hello("Olesya");
 
+    double l = 5.0;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+    double a = 6.0;
+    double b = 4.0;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double l) {
+    return l * l;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
   }
 
 }
