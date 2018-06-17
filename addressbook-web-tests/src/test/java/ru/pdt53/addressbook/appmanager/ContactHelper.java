4dff4a6967922926e9913f5f77c8165a388609d0
OlesyaF <sorglos_engel@mail.ru>
2018-06-17 23:24:44 +0300

    Задание 5. Реализована двухуровневая архитектура тестового набора. Создан общий базовый класс BaseHelper

--- addressbook-web-tests/src/test/java/ru/pdt53/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/pdt53/addressbook/appmanager/ContactHelper.java
@@ -4,33 +4,24 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.pdt53.addressbook.model.ContactData;
 
-public class ContactHelper {
-  private FirefoxDriver wd;
+public class ContactHelper extends BaseHelper {
 
   public ContactHelper(FirefoxDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void saveNewContact() {
-      wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    click(By.xpath("//div[@id='content']/form/input[21]"));
   }
 
   public void fillNewAddForm(ContactData personData) {
-      wd.findElement(By.name("firstname")).click();
-      wd.findElement(By.name("firstname")).clear();
-      wd.findElement(By.name("firstname")).sendKeys(personData.getFirstname());
-      wd.findElement(By.name("lastname")).click();
-      wd.findElement(By.name("lastname")).clear();
-      wd.findElement(By.name("lastname")).sendKeys(personData.getLastname());
-      wd.findElement(By.name("mobile")).click();
-      wd.findElement(By.name("mobile")).clear();
-      wd.findElement(By.name("mobile")).sendKeys(personData.getMobile());
-      wd.findElement(By.name("email")).click();
-      wd.findElement(By.name("email")).clear();
-      wd.findElement(By.name("email")).sendKeys(personData.getEmail());
+    type(By.name("firstname"), personData.getFirstname());
+    type(By.name("lastname"), personData.getLastname());
+    type(By.name("mobile"), personData.getMobile());
+    type(By.name("email"), personData.getEmail());
   }
 
   public void gotoNewAddPage() {
-      wd.findElement(By.linkText("add new")).click();
+    click(By.linkText("add new"));
   }
 }
