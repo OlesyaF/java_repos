bc7e62e5942a30d29d3bb9d85b7963e6f68e9657
OlesyaF <sorglos_engel@mail.ru>
2018-06-24 00:26:53 +0300

    В тесты web-приложения добавлена возможность выбора драйвера (Firefox, Chrome, IE)

--- addressbook-web-tests/src/test/java/ru/pdt53/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/pdt53/addressbook/tests/TestBase.java
@@ -3,10 +3,11 @@ package ru.pdt53.addressbook.tests;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.pdt53.addressbook.appmanager.ApplicationManager;
+import sun.plugin2.util.BrowserType;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(org.openqa.selenium.remote.BrowserType.IE);
 
   @BeforeMethod
   public void setUp() throws Exception {
