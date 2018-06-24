b3741c59069856098ca078cebd96d0f61d7c1ef8
OlesyaF <sorglos_engel@mail.ru>
2018-06-24 17:13:42 +0300

    В тестах Addressbook реализована проверка совпадения вводимых в поля значений и текущих

--- addressbook-web-tests/src/test/java/ru/pdt53/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/pdt53/addressbook/tests/GroupModificationTests.java
@@ -10,7 +10,7 @@ public class GroupModificationTests extends TestBase {
     app.getNavigationHelper().gotoGroupPage();
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("group2", "group_header2", "comment2"));
+    app.getGroupHelper().fillGroupForm(new GroupData("groupA", null, "comment2"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
   }
