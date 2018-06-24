daacb30235c06e744e883abe87b6c760707e969b
OlesyaF <sorglos_engel@mail.ru>
2018-06-25 00:17:46 +0300

    В NavigationHelper добавлен метод перехода на домашнюю страницу и проверки на нахождение на требуемой странице

--- addressbook-web-tests/src/test/java/ru/pdt53/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/pdt53/addressbook/tests/GroupCreationTests.java
@@ -11,7 +11,7 @@ public class GroupCreationTests extends TestBase{
         app.getGroupHelper().initGroupCreation();
         app.getGroupHelper().fillGroupForm(new GroupData("group_min", null, "commentM"));
         app.getGroupHelper().submitGroupCreation();
-        app.getGroupHelper().returnToGroupPage();
+        app.getNavigationHelper().gotoHomePage();
     }
 
 }
