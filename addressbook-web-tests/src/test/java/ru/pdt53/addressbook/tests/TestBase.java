5d6dd620476f4841fe741225e90ec098c5eb66e5
OlesyaF <sorglos_engel@mail.ru>
2018-07-01 23:23:43 +0300

    Добавлены функции для проверки простых чисел и тесты для них

--- addressbook-web-tests/src/test/java/ru/pdt53/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/pdt53/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package ru.pdt53.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.pdt53.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager(org.openqa.selenium.remote.BrowserType.FIREFOX);
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
   @BeforeMethod
   public void setUp() throws Exception {
