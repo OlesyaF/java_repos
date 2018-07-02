eab8b0048de4788806b2e77c50b0f4bd637c1d76
OlesyaF <sorglos_engel@mail.ru>
2018-07-02 03:57:15 +0300

    В тесты для групп добавлено получение списка групп

--- addressbook-web-tests/src/test/java/ru/pdt53/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/pdt53/addressbook/tests/GroupDeletionTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.pdt53.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupDeletionTests extends TestBase {
 
   @Test
@@ -12,12 +14,12 @@ public class GroupDeletionTests extends TestBase {
     if (!app.getGroupHelper().isThereGroup()) {
       app.getGroupHelper().createGroup(new GroupData("group_del", "header_del", "comment_del"));
     }
-    int before = app.getGroupHelper().getGroupCount();
-    app.getGroupHelper().selectGroup(before - 1);
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().deleteSelectedGroups();
     app.getNavigationHelper().gotoGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before - 1);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() - 1);
   }
 
 }
