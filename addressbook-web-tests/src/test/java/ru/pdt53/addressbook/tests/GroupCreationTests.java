2e81cbf0608fa429c4f894685055f4db5f8c3966
OlesyaF <sorglos_engel@mail.ru>
2018-07-20 01:39:49 +0300

    Для теста создания групп добавлен вывод логов в консоль и в лог-файл

--- addressbook-web-tests/src/test/java/ru/pdt53/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/pdt53/addressbook/tests/GroupCreationTests.java
@@ -3,6 +3,8 @@ package ru.pdt53.addressbook.tests;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.pdt53.addressbook.model.GroupData;
@@ -20,6 +22,8 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
+
+  Logger logger = LoggerFactory.getLogger(GroupCreationTests.class);
 
   @DataProvider
   public Iterator<Object[]> validGroupsFromXml() throws IOException {
@@ -55,6 +59,7 @@ public class GroupCreationTests extends TestBase {
 
   @Test(dataProvider = "validGroupsFromJson")
   public void testGroupCreation(GroupData group) {
+    logger.info("Start test testGroupCreation");
     app.goTo().groupPage();
     Groups before = app.group().all();
     app.group().create(group);
@@ -62,6 +67,7 @@ public class GroupCreationTests extends TestBase {
     Groups after = app.group().all();
     assertThat(after, equalTo(
             before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+    logger.info("Stop test testGroupCreation");
   }
 
   @Test(enabled = false) //проверяет, что нельзя создать группу с апострофом в названии
