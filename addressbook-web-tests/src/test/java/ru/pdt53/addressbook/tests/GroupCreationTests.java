b0d3bd9d1c702e45570f5c4fab929e19e9dbf6b1
OlesyaF <sorglos_engel@mail.ru>
2018-07-19 17:49:48 +0300

    Реализована загрузка и выгрузка данных в формате xml для создания групп

--- addressbook-web-tests/src/test/java/ru/pdt53/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/pdt53/addressbook/tests/GroupCreationTests.java
@@ -1,14 +1,18 @@
 package ru.pdt53.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.pdt53.addressbook.model.GroupData;
 import ru.pdt53.addressbook.model.Groups;
 
-import java.io.*;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -17,18 +21,20 @@ public class GroupCreationTests extends TestBase {
 
   @DataProvider
   public Iterator<Object[]> validGroups() throws IOException {
-    List<Object[]> list = new ArrayList<Object[]>();
-    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
+    String xml = "";
     String line = reader.readLine();
     while (line != null) {
-      String[] split = line.split(";");
-      list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+      xml += line;
       line = reader.readLine();
     }
-    return list.iterator();
+    XStream xstream = new XStream();
+    xstream.processAnnotations(GroupData.class);
+    List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
+    return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
   }
 
-  @Test (dataProvider = "validGroups")
+  @Test(dataProvider = "validGroups")
   public void testGroupCreation(GroupData group) {
     app.goTo().groupPage();
     Groups before = app.group().all();
@@ -39,7 +45,7 @@ public class GroupCreationTests extends TestBase {
             before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
 
-  @Test (enabled = false) //проверяет, что нельзя создать группу с апострофом в названии
+  @Test(enabled = false) //проверяет, что нельзя создать группу с апострофом в названии
   public void testBadGroupCreation() {
     app.goTo().groupPage();
     Groups before = app.group().all();
