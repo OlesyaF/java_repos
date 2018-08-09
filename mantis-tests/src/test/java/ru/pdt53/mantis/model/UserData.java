eee912421df659d06993c29e1334481652cfc834
OlesyaF <sorglos_engel@mail.ru>
2018-08-09 20:01:33 +0300

    В тесте смены пароля у пользователя MantisBT выбор пользователя реализован через обращение к БД

---
+++ mantis-tests/src/test/java/ru/pdt53/mantis/model/UserData.java
@@ -0,0 +1,61 @@
+package ru.pdt53.mantis.model;
+
+import java.util.Objects;
+
+public class UserData {
+
+  private int id = Integer.MAX_VALUE;
+  private String username;
+  private String email;
+
+  public UserData withId(int id) {
+    this.id = id;
+    return this;
+  }
+
+  public UserData withUsername(String username) {
+    this.username = username;
+    return this;
+  }
+
+  public UserData withEmail(String email) {
+    this.email = email;
+    return this;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    UserData userData = (UserData) o;
+    return id == userData.id &&
+            Objects.equals(username, userData.username) &&
+            Objects.equals(email, userData.email);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, username, email);
+  }
+
+  @Override
+  public String toString() {
+    return "UserData{" +
+            "id=" + id +
+            ", username='" + username + '\'' +
+            ", email='" + email + '\'' +
+            '}';
+  }
+}
--- mantis-tests/src/test/java/ru/pdt53/mantis/tests/ChangePasswordTests.java
+++ mantis-tests/src/test/java/ru/pdt53/mantis/tests/ChangePasswordTests.java
@@ -5,9 +5,13 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.lanwen.verbalregex.VerbalExpression;
 import ru.pdt53.mantis.model.MailMessage;
+import ru.pdt53.mantis.model.UserData;
+import ru.pdt53.mantis.model.Users;
 
 import javax.mail.MessagingException;
 import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.testng.Assert.assertTrue;
@@ -26,17 +30,49 @@ public class ChangePasswordTests extends TestBase {
     String admin_password = "root";
 
     String new_password = "new_password123";
-    String user = "auser1533596398918";
-    String email = "user1533596398918@localhost.localdomain";
+    String username = null;
+    String email = null;
+    UserData user = null;
 
-    app.change_pass().change_init(admin, admin_password, user);
+    Connection conn = null;
+
+    try {
+      conn = DriverManager.getConnection("jdbc:mysql://localhost/bugtracker?user=root&password=&serverTimezone=UTC");
+      Statement st = conn.createStatement();
+      ResultSet rs = st.executeQuery(
+              "select id, username, email from mantis_user_table where username <> 'administrator' and username like 'a%' order by id desc");
+      Users users = new Users();
+      while (rs.next()) {
+        users.add(new UserData()
+                .withId(rs.getInt("id"))
+                .withUsername(rs.getString("username"))
+                .withEmail(rs.getString("email")));
+      }
+
+      rs.close();
+      st.close();
+      conn.close();
+
+      user = users.iterator().next();
+      username = user.getUsername();
+      email = user.getEmail();
+
+    } catch (SQLException ex) {
+      System.out.println("SQLException: " + ex.getMessage());
+      System.out.println("SQLState: " + ex.getSQLState());
+      System.out.println("VendorError: " + ex.getErrorCode());
+    }
+
+    System.out.println("Пользователь, у которого тест меняет пароль: " + user);
+
+    app.change_pass().change_init(admin, admin_password, username);
 
     List<MailMessage> mailMessages = app.mail().waitForMail(1, 10000);
     String confirmationLink = findConfirmationLink(mailMessages, email);
 
     app.change_pass().change_confirm(confirmationLink, new_password);
 
-    assertTrue(app.newSession().login(user, new_password));
+    assertTrue(app.newSession().login(username, new_password));
   }
 
   private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
