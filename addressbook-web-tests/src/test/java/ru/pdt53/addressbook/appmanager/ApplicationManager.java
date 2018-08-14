81d2bc2b176612eeb5d94fb41d2f6f468a04ad2d
OlesyaF <sorglos_engel@mail.ru>
2018-08-14 16:03:00 +0300

    Реализована возможность удаленного запуска браузера

--- addressbook-web-tests/src/test/java/ru/pdt53/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/pdt53/addressbook/appmanager/ApplicationManager.java
@@ -5,10 +5,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -35,13 +38,22 @@ public class ApplicationManager {
 
     dbHelper = new DbHelper();
 
-    if (browser.equals(org.openqa.selenium.remote.BrowserType.FIREFOX)) {
-      wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-    } else if (browser.equals(org.openqa.selenium.remote.BrowserType.CHROME)) {
-      wd = new ChromeDriver();
-    } else if (browser.equals(org.openqa.selenium.remote.BrowserType.IE)) {
-      wd = new InternetExplorerDriver();
+    if ("".equals(properties.getProperty("selenium.server"))) {
+
+      if (browser.equals(org.openqa.selenium.remote.BrowserType.FIREFOX)) {
+        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+      } else if (browser.equals(org.openqa.selenium.remote.BrowserType.CHROME)) {
+        wd = new ChromeDriver();
+      } else if (browser.equals(org.openqa.selenium.remote.BrowserType.IE)) {
+        wd = new InternetExplorerDriver();
+      }
+
+    } else {
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setBrowserName(browser);
+      wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
+
     wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
     wd.get(properties.getProperty("web.baseUrl"));
     contactHelper = new ContactHelper(wd);
